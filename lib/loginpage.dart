9ed3ff55ad6233cf4dba56210631d04adcfe9cfd
ekanshmittal <b422026@iiit-bh.ac.in>
2024-03-28 15:11:13 +0530

    Day 2 - Flutter

---
+++ lib/loginpage.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: use_key_in_widget_constructors, annotate_overrides, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+
+      Widget build(BuildContext context){
+        return Material(
+          child : Center(
+            child : Text('Login Page')
+          )
+        );
+      }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_dart/homepage.dart';
+import 'package:flutter_dart/loginpage.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,11 +14,25 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-       home: Homepage()
+      // home: Homepage()
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      darkTheme: ThemeData(primaryColor: Colors.black12),
+    routes:{
+      "/": (context) => Homepage(),
+      "/home" : (context)  => Homepage(),
+      "/login " :(context) => LoginPage() 
+    }
     );
   }
 }
 
-//understand Widgets as tree of composables
-//the above homepage content is preferablly written in seprate document
-//replace the code with class Homepage
+//themes are used to set the theme of the app when used in the ight mode
+// it's corresponding darkTheme is used to theme App in DarkmOde
+//routes are used to control the structure of the application 
+// "/" is the default homepage , but this can be changed using intialroute
+
+void bringveggies({int money = 100 , @required carrybag}){
+     //{} are used to define optional params which have some default values , if there is no default val
+     // then @required is used
+}

2f2a608366d2ef18c2954ee5e55f4ae704753a97
ekanshmittal <b422026@iiit-bh.ac.in>
2024-04-01 00:31:22 +0530

    Day3 - Flutter

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,14 +1,61 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, avoid_renaming_method_parameters
+// ignore_for_file: use_key_in_widget_constructors, annotate_overrides, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext build) {
-    return Material(
-      child: Center(
-        child:  Text('Login page')
-      ),
-    );
-  }
+class LoginPage extends StatelessWidget{
+
+      Widget build(BuildContext context){
+        return Material(
+          child : Center(
+            child : Column(
+              children: [
+                Image.asset('assets/images/login_image.png'),
+
+                SizedBox(height: 16,),
+                
+                Text('Login/Sign Up'),
+
+                SizedBox(height: 16,),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0 , horizontal: 8.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Username",
+                          label: Text('Username'),
+                          border: OutlineInputBorder(borderSide: BorderSide(width: 2))
+                          )
+                      ),
+                      SizedBox(height : 8),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Enter Password",
+                          label: Text('Password'),
+                          border: OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                          ),
+                        obscureText: true,
+                        obscuringCharacter: '*',  
+                      ),
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: 16),
+
+                ElevatedButton(
+                  onPressed: (){} ,
+                  child: Text('Login'),
+      
+                )
+            
+
+              ],
+            )
+          )
+        );
+      }
 }
+
+//study about buttons , textbox , columns , image , adding text style
